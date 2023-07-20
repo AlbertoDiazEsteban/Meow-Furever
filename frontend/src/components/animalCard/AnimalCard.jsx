f854a554e4ec631348e182604ddd43830779acf8
LuLu <lu.luzan@gmail.com>
2023-07-20 09:37:33 +0200

    card_style

--- frontend/src/components/animalCard/AnimalCard.jsx
+++ frontend/src/components/animalCard/AnimalCard.jsx
@@ -1,22 +1,18 @@
 import "./animalCard.css";
 
-
-
-
 function AnimalCard(props) {
   return (
     <>
-   
-   <section className="aC-cardContainer">
-        <img id="aC-imgContainer" src= {props.image} alt="perrete"/>
-    
-      <div className="ac-cardText">
-        <h3 className={props.title}>{props.contentTitle}</h3>
-        <hr />
-        <p className={props.text}>{props.contentText}</p>
+      <div className="aC-cardContainer">
+          <img id="aC-imgContainer" src= {props.image} alt="perrete"/>
+          <p>Hola</p>
+        <div className="ac-cardText">
+          <h3 className={props.title}>{props.contentTitle}</h3>
+          <hr />
+          <p className={props.text}>{props.contentText}</p>
+        </div>
+        
       </div>
-      
-    </section>
     </>
   )
 }
