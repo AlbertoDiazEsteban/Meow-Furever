9ec0895bc034d9ff9044b92e45e38d28093d5363
andreina <a.fernandez101092@gmail.com>
2023-07-20 17:52:53 +0200

    colores del contenedor principal dropdown

--- frontend/src/components/pages/landingPage/LandingPage.jsx
+++ frontend/src/components/pages/landingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import Banner from "../../Banner/Banner";
 import Header from "../../Header/Header";
-import AnimalsList from "../../animalsList/AnimalsList"import Dropdown from "../Dropdown/Dropdown"
+import AnimalsList from "../../animalsList/AnimalsList";
+import Dropdown from "../Dropdown/Dropdown"
 
 function LandingPage() {
   return (
